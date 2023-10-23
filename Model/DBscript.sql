767493c9bf0c41e2e2383a54cdefc2c84c1b0674
antonioHCM <antoniohmagalhaes@hotmail.com>
2023-10-23 19:23:38 +0200

    Sql script complete

--- Model/DBscript.sql
+++ Model/DBscript.sql
@@ -36,3 +36,41 @@ CREATE TABLE Product(
     price FLOAT,
     description TEXT
 )ENGINE=InnoDB;
+
+CREATE TABLE `Order`(
+    orderID INT AUTO_INCREMENT NOT NULL  PRIMARY KEY,
+    orderAmount INT,
+    currentStatus VARCHAR(50),
+    orderDate DATE
+)ENGINE=InnoDB;
+
+CREATE TABLE Payment(
+    paymentID INT AUTO_INCREMENT NOT NULL  PRIMARY KEY,
+    orderID INT,
+    FOREIGN KEY (orderID) REFERENCES `Order` (orderID),
+    paymentsType VARCHAR(50),
+    paymentDate DATE
+)ENGINE=InnoDB;
+
+CREATE TABLE OrderItem(
+    orderItemID INT AUTO_INCREMENT NOT NULL  PRIMARY KEY,
+    orderID INT,
+    FOREIGN KEY (orderID) REFERENCES `Order` (orderID),
+    userID INT,
+    FOREIGN KEY (userID) REFERENCES `User` (userID),
+    productID INT,
+    FOREIGN KEY (productID) REFERENCES Product (productID),
+    price FLOAT NOT NULL,
+    quantity INT NOT NULL
+)ENGINE=InnoDB;
+
+CREATE TABLE Review(
+    reviewID INT AUTO_INCREMENT NOT NULL  PRIMARY KEY,
+    productID INT,
+    FOREIGN KEY (productID) REFERENCES Product (productID),
+    userID INT,
+    FOREIGN KEY (userID) REFERENCES User (userID),
+    rating INT,
+    reviewDate Date NOT NULL,
+    reviewContent TEXT NOT NULL
+)ENGINE=InnoDB;
